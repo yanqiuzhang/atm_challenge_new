b3fdf3f6b7216667baa890c13bbdf98c418d151e
olivia <olivia@evolve.se>
2019-01-22 11:30:09 +0100

    modified the atm_spec.rb and add a new success message

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -8,7 +8,6 @@ describe Atm do
         allow(account).to receive(:balance=)
     end
 
-
     it 'has $1000 on initialize' do
         expect(subject.funds).to eq 1000
     end
@@ -16,5 +15,10 @@ describe Atm do
     it 'funds are reduced at withdraw' do
         subject.withdraw(50)
         expect(subject.funds).to eq 950
+    end
+
+    it 'allow withdraw if account has enough balance.' do
+         ecpected_output = { status: true, message: 'success', date: Date.today, amount: 45}
+         expect(subject.withdraw(45, account)).to eq expected_output
     end
 end
