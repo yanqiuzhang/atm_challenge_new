f6a38c68e5100791facfe42ae365550076c938ae
olivia <olivia@evolve.se>
2019-01-24 17:03:46 +0100

    modified wrong

--- lib/account.rb
+++ lib/account.rb
@@ -1,10 +1,13 @@
 require 'date'
 
 class Account 
-
-    attr_accessor  :pin_code
+    attr_accessor  :pin_code, :exp_date , :account_status, :balance
     
     def initialize(attrs = {}) 
-        @pin_code = rand(1000..9999).to_s 
+        @pin_code = rand(1000..9999).to_i
+        @exp_date = '%d/%m'
+        @account_status = :active
+        @balance = 100
+
     end
 end
