5c14e0342533bf242e50458fd9edd19b7d420857
olivia <olivia@evolve.se>
2019-01-24 11:57:25 +0100

    solved bills feature

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -20,7 +20,7 @@ describe Atm do
 
      it 'allow withdraw if account has enough balance.' do
         expected_output = { status: true, message: 'success', date: Date.today, amount: 45, bills: [20, 20, 5]}
-        expect(subject.withdraw(45, '1234', account, add_bills(amount))).to eq expected_output
+        expect(subject.withdraw(45, '1234', account)).to eq expected_output
    end
 
     it 'rejects withdraw if account has insufficient funds.' do
@@ -50,15 +50,5 @@ describe Atm do
         expected_output = {status: false, message: 'account is disabled', date: Date.today }
         expect(subject.withdraw(15, '1234', account)).to eq expected_output
     end
-    it 'allow withdraw if atm has enough balance.' do
-        expected_output = {
-            status: true,
-            message: 'success',
-            date: Date.today,
-            amount: 45,
-            bills: [20, 20, 5]}
-
-        expect(subject.withdraw(45, '1234', account, add_bills(amount))).to eq expected_output
-
 
 end
