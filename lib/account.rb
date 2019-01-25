e5bc6eb3f6c6c888799e39ec8e0f5be6e2b6d9a0
olivia <olivia@evolve.se>
2019-01-25 14:05:20 +0100

    dev

--- lib/account.rb
+++ lib/account.rb
@@ -2,19 +2,30 @@ require 'date'
 
 class Account 
 
-    attr_accessor  :pin_code, :exp_date, :account_status, :balance
+    attr_accessor  :pin_code, :exp_date, :account_status, :balance, 
         
     def initialize(attrs = {}) 
         @pin_code = rand(1000..9999).to_i 
         @exp_date = Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
         @balance = 100
         @account_status = :active
+        @owner = set_owner(attrs[:owner])
 
     end
 
 
     def deactivate
-        
+        @account_status = :deactivated
     end
+
+    private 
+
+    def set_owner(obj)
+        obj == nil ?  missing_owner : @owner = obj
+      end
+      
+      def missing_owner
+        raise "An Account owner is required"
+      end
 
 end
